5e79cc288226e8573d43859c371eb71f449fc156
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-05-10 22:34:16 +0200

    change dot

--- main.js
+++ main.js
@@ -15,13 +15,24 @@ const sliders = [{
 
 const img = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')]
+
 let active = 0;
+
+const changeDot = () => {
+   const activeDot = dots.findIndex((dot)=>dot.classList.contains('active'));
+   dots[activeDot].classList.remove('active')
+   dots[active].classList.add('active')
+}
 
 const changeSlide = () => {
     active++;
     if (active === sliders.length) active = 0
     img.src = sliders[active].image;
     h1.textContent = sliders[active].txt;
+    changeDot();
 }
-    
+
+
+
 setInterval(changeSlide, 2000)
